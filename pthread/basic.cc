20be5388d19bc0948978b45f39027884fa8c1098
darren <dxxx0730@foxmail.com>
2013-03-21 16:27:56 +0800

    detach

--- pthread/basic.cc
+++ pthread/basic.cc
@@ -13,8 +13,19 @@ typedef struct
 
 void* basic_thread(void* args)
 {
+    int rt;
+    if((rt = pthread_detach(pthread_self())))
+    {
+        fprintf(stderr, "[thread %d] call pthread_detach fail! error code is %d .", pthread_self(), rt);
+        return NULL;
+    }
+    // 该函数用于指明用于线程thread的存储空间可以在该线程终止后再利用
+    // 如果thread未停止，pthread_detach()不会导致它终止
+    // 对同一个线程多次调用pthread_detach()的行为未定义
+
     thread_args* arg = (thread_args*) args;
     printf("[thread %d]  output is : %s\n", arg->thread_id, arg->str);
+    return NULL;
 }
 
 void basic_use()
