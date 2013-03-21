17898568c079e7e6f1484ddfb717e53ff3c41d8a
darren <dxx0730@foxmail.com>
2013-03-21 13:44:42 +0800

    easy

--- easy/main.cc
+++ easy/main.cc
@@ -1,8 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <easy_client.h>
+#include <easy_io.h>
 
 int main()
 {
+    if(easy_io_create(1))
+    {
+        fprintf(stderr, "easy_io_create error.\n");
+    }
     return 0;
 }
